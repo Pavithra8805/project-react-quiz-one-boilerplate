30d11e8650dde0c50639c5500f8d2d2cdec6420e
Pavithra8805 <pavithra8805@gmail.com>
2024-01-24 17:44:06 +0000

    first commit

---
+++ quiz/src/App.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Result from './Componnents/ResultComponent/Result';
+import Quiz from "./Componnents/QuizComponent/Quiz";
+import Home from './Componnents/HomeComponent/Home';
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <Quiz />
+      {/* <Result /> */}
+
+    </div>
+  );
+}
+
+export default App;
